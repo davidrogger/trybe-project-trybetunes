4d0563f580df56befe42adbe00caf700764fda46
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-05 09:19:39 -0300

    Requisito 1 - usando BrowserRouter com Route para definir o caminho de cada componente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+// Bibliotecas
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends React.Component {
+// Componentes
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
+
+class App extends Component {
   render() {
-    return (<p>TrybeTunes Dvd</p>);
+    return (
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile/" component={ Profile } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/search" component={ Search } />
+            <Route exact path="/" component={ Login } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </Router>
+      </main>
+    );
   }
 }
 
