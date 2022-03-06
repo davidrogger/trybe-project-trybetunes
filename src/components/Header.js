e1e8df4bfddc658f119bc3dd392c89bcfc2231fe
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-06 08:04:21 -0300

    criacao do componente header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,43 @@
+// Bibliotecas
 import React, { Component } from 'react';
 
+// Serviços
+import { getUser } from '../services/userAPI';
+
+// Componentes
+import Loading from './Loading';
+
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: '',
+      headerLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.userLoged();
+  }
+
+  userLoged = async () => {
+    const userProfile = await getUser();
+    const { name } = userProfile;
+    this.setState({
+      userName: name,
+      headerLoading: false,
+    });
+  }
+
   render() {
-    return (
-      <header data-testid="header-component">
-        <h1>Header</h1>
+    const { userName, headerLoading } = this.state;
+    return headerLoading ? <Loading /> : (
+      <header data-testid="header-component" className="header-container">
+        <h1>trybe tunes</h1>
+        <section>
+          <h2 data-testid="header-user-name">{userName}</h2>
+        </section>
       </header>
     );
   }
