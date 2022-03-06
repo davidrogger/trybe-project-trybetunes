35ed02e38559271c82420946213f3ccf8239a45b
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-06 09:53:26 -0300

    generalizando estilo do botao desabilitado e aplicando logica do requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,19 +5,53 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchFor: '',
+      buttonDisable: true,
+    };
+  }
+
+searchStateUpdate = ({ target }) => {
+  const { value } = target;
+  this.setState({
+    searchFor: value,
+  }, () => this.minSearchLength());
+}
+
+  minSearchLength = () => {
+    const { searchFor } = this.state;
+    const minLength = 2;
+    const lengthValidation = searchFor.length >= minLength;
+    this.setState({
+      buttonDisable: !lengthValidation,
+    });
+  }
+
   render() {
+    const { searchFor, buttonDisable } = this.state;
     return (
       <section data-testid="page-search" className="top-container">
         <Header />
         <section className="search-container">
 
-          <input type="text" data-testid="search-artist-input" />
+          <input
+            type="text"
+            placeholder="Nome do Artista ou Banda"
+            data-testid="search-artist-input"
+            value={ searchFor }
+            onChange={ this.searchStateUpdate }
+          />
 
           <button
             type="button"
             className="blue-button"
+            data-testid="search-artist-button"
+            disabled={ buttonDisable }
           >
-            Procurar
+            Pesquisar
 
           </button>
 
