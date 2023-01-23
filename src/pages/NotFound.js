436cd96711810dde781325ba8c908f34d2bd2395
Davíd Roggér <davidrogger.a@gmail.com>
2023-01-23 07:16:59 -0300

    style: not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+import logo from '../imgs/trybetunes_logo.svg';
+
 class NotFound extends Component {
   render() {
     return (
-      <section data-testid="page-not-found">
-        <h1>NotFound</h1>
+      <section
+        className="notfound-container"
+        data-testid="page-not-found"
+      >
+        <img src={ logo } alt="trybetunes logo" />
+        <section className="notfound-msg-container">
+          <span className="notfound-title">
+            Ops!
+          </span>
+          <span className="notfound-msg ">
+            A página que você está procurando não foi encontrada
+          </span>
+        </section>
       </section>
     );
   }
